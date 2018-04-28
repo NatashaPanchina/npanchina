0afab6d5fe91f1ee161cbb192f3f2a47e800afb8
NatashaPanchina <panchinanata25@gmail.com>
2018-04-28 21:30:54 +0600

    1.4. JUnit

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,9 +1,11 @@
 package ru.job4j;
+
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 /**
   * Test.
   *
@@ -13,21 +15,14 @@ import static org.junit.Assert.assertThat;
   */
 public class CalculateTest {
   /**
-    * Test add.  
+    * Test echo.  
     */
   @Test
-  public void whenAddOneToOneThenTwo() {
-     ByteArrayOutputStream out = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(out));
-     Calculate.main(null);
-     assertThat(
-              out.toString(),
-              is(
-                    String.format(
-                             "Hello, world%s",
-                             System.getProperty("line.separator")
-                    )
-               )
-     );
+  public void whenTakeNameThenTreeEchoPlusName(){
+	  String input = "Petr Arsentev";
+	  String expect = "Echo, echo, echo : Petr Arsentev";
+	  Calculate calc = new Calculate();
+	  String result = calc.echo(input);
+	  assertThat(result, is(expect));
   }
 }
