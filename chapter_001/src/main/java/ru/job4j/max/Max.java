b68c717f1b724ba560bc07f51bc57c1a04c5762c
NatashaPanchina <panchinanata25@gmail.com>
2018-05-12 18:46:33 +0600

    3.2. Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.max;
+
+/**
+ * Maximum.
+ *
+ * @author Natasha Panchina (panchinanata25@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Method max
+     * @param first number
+     * @param second number
+     * @return maximum
+     */
+    public int max(int first, int second) {
+        return  first > second ? first : second;
+    }
+}
