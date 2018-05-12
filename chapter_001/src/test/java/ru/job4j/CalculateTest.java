d4ebe0def2c3baf177aa4548bf37f4128cb65ebf
NatashaPanchina <panchinanata25@gmail.com>
2018-05-12 18:23:21 +0600

    Whitespace

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-  * Test.
-  *
-  * @author Natasha Panchina (panchinanata25@gmail.com)
-  * @version $Id$
-  * @since 0.1
-  */
+ * Test.
+ *
+ * @author Natasha Panchina (panchinanata25@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-  /**
-    * Test echo.  
-    */
-  @Test
-  public void whenTakeNameThenTreeEchoPlusName() {
-	  String input = "Petr Arsentev";
-	  String expect = "Echo, echo, echo : Petr Arsentev";
-	  Calculate calc = new Calculate();
-	  String result = calc.echo(input);
-	  assertThat(result, is(expect));
-  }
+	/**
+	 * Test echo.
+	 */
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "Petr Arsentev";
+		String expect = "Echo, echo, echo : Petr Arsentev";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
 }
