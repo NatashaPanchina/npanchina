aa7ff3310dcb74bcc7e7638f695ef24190093bbe
NatashaPanchina <panchinanata25@gmail.com>
2018-06-02 19:55:50 +0600

    5.3. Построить шахматную доску в псевдографике.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+/**
+ * Board.
+ *
+ * @author Natasha Panchina (panchinanata25@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+    /**
+     * Method paint.
+     * @param width
+     * @param height
+     * @return paint board
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.getProperty("line.separator");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("x");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return  screen.toString();
+    }
+}
+
