f9f8ebd45a3d5b50a13f55c2ab924b2553fa398e
Natasha <panchinanata25@gmail.com>
2019-06-21 19:14:09 +0600

    4. Переопределить только equals

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -6,7 +6,7 @@ import java.util.Calendar;
  * User.
  *
  * @author Natasha Panchina (panchinanata25@gmail.com)
- * @version 2
+ * @version 3
  * @since 11.06.2019
  */
 public class User {
@@ -20,12 +20,29 @@ public class User {
         this.birthday = birthday;
     }
 
-    @Override
+    /**@Override
     public int hashCode() {
         int result = 1;
         result = result * 31 + (this.name == null ? 0 : this.name.hashCode());
         result = result * 31 + this.children;
         result = result * 31 + (this.birthday == null ? 0 : this.birthday.hashCode());
         return result;
+    }
+     **/
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof User)) {
+            return false;
+        }
+        User user = (User) other;
+        return (this.children == user.children)
+                && (this.name == null ? user.name == null
+                    : this.name.equals(user.name))
+                && (this.birthday == null ? user.birthday == null
+                    : this.birthday.equals(user.birthday));
     }
 }
